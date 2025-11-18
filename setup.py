aef128428c0fd1872716eb84c195a7b2bb30b305
Arco <678514+arcidodo@users.noreply.github.com>
2025-11-18 20:41:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,22 +6,23 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='cbpi4-HA-Actor',
+setup(name='cbpi4-HA-Sensor',
       version='0.0.1',
-      description='CraftBeerPi4 Home Assistant Actor Plugin',
+      description='CraftBeerPi4 Home Assistant Sensor Plugin',
       author='Jan Gebauer',
       author_email='mail@jan-gebauer.de',
-      url='https://github.com/gebauer/cbpi4-HA-Actor',
+      url='https://github.com/arcidodo/cbpi4-HA-Sensor',
       license='GPLv3',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
                   '': ['*.txt', '*.rst', '*.yaml'],
                   'cbpi4-HA-Actor': ['*', '*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4-HA-Actor'],
+      packages=['cbpi4-HA-Sensor'],
       install_requires=[
             'cbpi4>=4.0.0.34',
       ],
       long_description=long_description,
       long_description_content_type='text/markdown'
       )
+
